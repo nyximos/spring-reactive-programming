86abb3c4342c9eb50e2b5eb92376e290ca526d91
박소미 <nyximos26@10x10.co.kr>
2023-01-03 14:08:00 +0900

    reactivestreams 이용

---
+++ src/main/java/com/reactive/live/ch06/Ex01_PubSub.java
@@ -0,0 +1,62 @@
+package com.reactive.live.ch06;
+import lombok.extern.slf4j.Slf4j;
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Slf4j
+public class Ex01_PubSub {
+    public static void main(String[] args) {
+        Publisher<Integer> pub = new Publisher<Integer>() {
+            Iterable<Integer> iter = Stream.iterate(1, a -> a+1).limit(10).collect(Collectors.toList());
+
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {    // 구독이 일어나는 action
+                sub.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {           // 한번에 데이터 얼만큼??
+                        try {
+                            iter.forEach(s->sub.onNext(s)); // 모든 원소를 onNext 로 보낸다.
+                            sub.onComplete();               // 데이터를 다 넘겼으면 완료했다고 알려줌
+                        } catch (Throwable t) {
+                            sub.onError(t);
+                        }
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber<Integer> sub = new Subscriber<Integer>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+                log.debug("onSubscribe");
+                s.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                log.debug("onNext:{}", item);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                log.debug("onError:{}", throwable);
+            }
+
+            @Override
+            public void onComplete() {
+                log.debug("onComplete");
+            }
+        };
+
+        pub.subscribe(sub);
+    }
+}

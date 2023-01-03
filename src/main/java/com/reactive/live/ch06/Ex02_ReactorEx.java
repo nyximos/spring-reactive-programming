a64bcffa7536e2f38c9f84587b735fa32a6ad6a1
박소미 <nyximos26@10x10.co.kr>
2023-01-03 17:06:12 +0900

    update: Flux method

--- src/main/java/com/reactive/live/ch06/Ex02_ReactorEx.java
+++ src/main/java/com/reactive/live/ch06/Ex02_ReactorEx.java
@@ -4,12 +4,15 @@ import reactor.core.publisher.Flux;
 
 public class Ex02_ReactorEx {
     public static void main(String[] args) {
-        Flux.create(e -> {
+        Flux.<Integer>create(e -> {
             e.next(1);
             e.next(2);
             e.next(3);
             e.complete();
         })
+        .log()
+        .map(s->s*10)
+        .reduce(0, (a,b)->a+b)
         .log()
         .subscribe(System.out::println);
     }

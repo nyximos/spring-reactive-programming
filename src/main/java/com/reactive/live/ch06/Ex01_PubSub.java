6c473e48553a993c92db02ee3ae16dd1d4cece5f
박소미 <nyximos26@10x10.co.kr>
2023-01-03 14:49:05 +0900

    refactoring

--- src/main/java/com/reactive/live/ch06/Ex01_PubSub.java
+++ src/main/java/com/reactive/live/ch06/Ex01_PubSub.java
@@ -4,37 +4,20 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Slf4j
 public class Ex01_PubSub {
     public static void main(String[] args) {
-        Publisher<Integer> pub = new Publisher<Integer>() {
-            Iterable<Integer> iter = Stream.iterate(1, a -> a+1).limit(10).collect(Collectors.toList());
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, a -> a + 1).limit(10).collect(Collectors.toList()));
 
-            @Override
-            public void subscribe(Subscriber<? super Integer> sub) {    // 구독이 일어나는 action
-                sub.onSubscribe(new Subscription() {
-                    @Override
-                    public void request(long n) {           // 한번에 데이터 얼만큼??
-                        try {
-                            iter.forEach(s->sub.onNext(s)); // 모든 원소를 onNext 로 보낸다.
-                            sub.onComplete();               // 데이터를 다 넘겼으면 완료했다고 알려줌
-                        } catch (Throwable t) {
-                            sub.onError(t);
-                        }
-                    }
+        pub.subscribe(logSub());
+    }
 
-                    @Override
-                    public void cancel() {
-
-                    }
-                });
-            }
-        };
-
-        Subscriber<Integer> sub = new Subscriber<Integer>() {
+    private static Subscriber<Integer> logSub() {
+        return new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription s) {
                 log.debug("onSubscribe");
@@ -56,7 +39,29 @@ public class Ex01_PubSub {
                 log.debug("onComplete");
             }
         };
+    }
 
-        pub.subscribe(sub);
+    private static Publisher<Integer> iterPub(List<Integer> iter) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {    // 구독이 일어나는 action
+                sub.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {           // 한번에 데이터 얼만큼??
+                        try {
+                            iter.forEach(s -> sub.onNext(s)); // 모든 원소를 onNext 로 보낸다.
+                            sub.onComplete();               // 데이터를 다 넘겼으면 완료했다고 알려줌
+                        } catch (Throwable t) {
+                            sub.onError(t);
+                        }
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
     }
 }
